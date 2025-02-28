a2bd8dc41872c2f2f098c4b527c0f9237b7f6df3
yuyuasd <980608294@qq.com>
2025-02-28 16:47:18 +0800

    优化庆祝动画的点击事件处理，调整粒子发射角度和层级

--- js/common.js
+++ js/common.js
@@ -9,19 +9,14 @@ async function loadComponent(elementId, path) {
 }
 
 function initConfetti() {
-    // 为整个文档添加点击事件监听
     document.addEventListener('click', (e) => {
-        const x = e.clientX / window.innerWidth;
-        const y = e.clientY / window.innerHeight;
-        
         confetti({
             particleCount: 100,
-            angle: 180,           
-            spread: 360,          
-            startVelocity: 15,    
-            gravity: 0.5,         
-            origin: { x, y },     
-            zIndex: 999,
+            spread: 70,
+            origin: { 
+                x: e.clientX / window.innerWidth,
+                y: e.clientY / window.innerHeight
+            },
             colors: [
                 '#E8C6C6', // morandi-pink
                 '#C6D5E8', // morandi-blue
@@ -29,8 +24,8 @@ function initConfetti() {
                 '#E8E6C6', // morandi-yellow
                 '#D6C6E8', // morandi-purple
                 '#D1D1D1', // morandi-gray
-                '#E8D8C6'  // morandi-beige
-            ]
+            ],
+            zIndex: 999999
         });
     });
 }
